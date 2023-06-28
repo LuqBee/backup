2d29dd7fff26b17fb04801cd0d59168dea35f3f6
Luke Beesley <131469362+LuqBee@users.noreply.github.com>
2023-06-28 17:37:15 -0600

    Add files via upload

---
+++ SeeSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SeeSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string GrifirithSimulatorStartup = "Welcome, to the Grifirith simulator! (100 million quadrillion % based off of this thing!!)";
+            string GrifirithSimulatorOne = "Choose your character!";
+            string GrifirithSimulatorTwo = "Type '1', for Grifirith. And that's it."; 
+
+
+
+            Console.WriteLine(GrifirithSimulatorStartup);
+            Console.WriteLine(GrifirithSimulatorOne);
+            Console.WriteLine(GrifirithSimulatorTwo);
+            Console.ReadLine(); // looks for answer
+
+            Console.WriteLine("Good Choice!");
+
+            Console.WriteLine("Grifirith, was a young lad.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
